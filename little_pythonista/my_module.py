a7b3d977d276757895fad40304b54a19fca893c9
Yuji Hamaguchi <yuji.hamaguchi@avanade.com>
2023-07-14 15:27:53 +0900

    init

---
+++ little_pythonista/my_module.py
@@ -0,0 +1,38 @@
+# pylint: disable=C0116
+from functools import reduce
+from operator import add
+from typing import Optional, Union
+
+# Q001: haskell の zip と同様の機能の関数 my_zip を書け （パラメータの数は可変であること）
+# zip :: [a] -> [b] -> [(a, b)]
+
+
+def my_zip(*xs):
+    if not xs:
+        return None
+    return list(map(lambda *x: tuple(x), *xs))
+
+# Q002: haskell の sum と同様の機能の関数 my_sum を書け。
+# sum :: (Num a) => [a] -> a
+# sum ns
+#     数値のリスト ns の総和を返す。
+#     see also: product, foldl
+#         sum [1, 2, 3]  = 6
+#         sum []         = 0
+
+
+def my_sum(ns):
+    if not ns:
+        return 0
+    return reduce(add, ns)
+
+# Q003: クイックソート関数 qsort01 を書け（リスト内包表記を使うこと）
+
+
+def qsort01(xs):
+    if not xs:
+        return []
+    x, *rest = xs
+    lt = [_x for _x in rest if _x < x]
+    ge = [_x for _x in rest if _x >= x]
+    return sum([qsort01(lt), [x], qsort01(ge)], [])
---
+++ tests/test_my_module.py
@@ -0,0 +1,38 @@
+# pylint: disable=C0116
+
+import unittest
+from little_pythonista.my_module import *
+
+
+class TestAdd(unittest.TestCase):
+    def test_my_zip(self):
+        self.assertEqual(my_zip([1, 2], "xy", "ab"), [
+                         (1, "x", "a"), (2, "y", "b")])
+        self.assertEqual(my_zip([1, 2], "x"), [(1, "x")])
+        self.assertEqual(my_zip([1], "xy"), [(1, "x")])
+        self.assertEqual(my_zip(), None)
+        self.assertEqual(my_zip(""), [])
+        self.assertEqual(my_zip("", [1]), [])
+        self.assertEqual(my_zip([1], ""), [])
+
+    def test_my_sum(self):
+        self.assertEqual(my_sum([1]), 1)
+        self.assertEqual(my_sum([1, 2]), 3)
+        self.assertEqual(my_sum([]), 0)
+
+    def test_qsort01(self):
+        self.assertEqual(qsort01([3, 2, 1]), [1, 2, 3])
+        self.assertEqual(qsort01([2, 3, 1]), [1, 2, 3])
+        self.assertEqual(qsort01([1, 2, 3]), [1, 2, 3])
+        self.assertEqual(qsort01([3, 2, 3, 1]), [1, 2, 3, 3])
+        self.assertEqual(qsort01("abc"), ["a", "b", "c"])
+        self.assertEqual(qsort01("cba"), ["a", "b", "c"])
+        self.assertEqual(qsort01("bca"), ["a", "b", "c"])
+        self.assertEqual(qsort01([1]), [1])
+        self.assertEqual(qsort01(["a"]), ["a"])
+        self.assertEqual(qsort01([]), [])
+        self.assertEqual(qsort01(""), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
