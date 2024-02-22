450acdee2e516943d292701c80512cbcc4a39a6d
Yuji Hamaguchi <yuji.hamaguchi@avanade.com>
2024-02-22 11:19:44 +0900

    tweak

--- sample.py
+++ sample.py
@@ -3,39 +3,3 @@ def double(x):
 
 def test_double():
     assert 4 == double(2)
-
-def plus_one(x):
-    return x + 1
-
-def test_plus_one():
-    assert 2 == plus_one(1)
-
-def minus_one(n):
-    return n - 1
-
-def test_minus_one():
-    assert 1 == minus_one(2)
-
-def plus_two(n):
-    return n + 2
-
-def test_plus_two():
-    assert 2 == plus_two(0)
-
-def minus_two(n):
-    return n - 2
-
-def test_minus_two():
-    assert 0 == minus_two(2)
-
-def function_b(n):
-    return n
-
-def test_function_b():
-    assert 1 == function_b(1)
-
-def function_c(n):
-    return n
-
-def test_function_c():
-    assert 1 == function_b(1)
