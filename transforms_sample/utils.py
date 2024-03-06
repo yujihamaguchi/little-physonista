a13c4aa1bc1a8d11c29c53897a02add9f43f4d2d
Yuji Hamaguchi <yuji.hamaguchi@avanade.com>
2024-03-06 16:52:51 +0900

    make a unit test failed

--- transforms_sample/utils.py
+++ transforms_sample/utils.py
@@ -4,13 +4,14 @@ import pytest
 def remove_leading_spaces(s: str) -> str:
     return s.lstrip()
 
+
 @pytest.mark.parametrize("test_input,expected", [
     ("  leading spaces", "leading spaces"),
     ("leading spaces  ", "leading spaces  "),
     ("\tleading tab", "leading tab"),
     ("   \t mixed spaces and tab", "mixed spaces and tab"),
     ("no_leading_space", "no_leading_space"),
-    ("　全角スペース", "全角スペース")
+    ("　全角スペース", "全角スペース ")
 ])
 def test_remove_leading_whitespace(test_input, expected):
     assert remove_leading_spaces(test_input) == expected
