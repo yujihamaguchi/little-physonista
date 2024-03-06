d6d488e466f7ad750cce291a9cb25fae729a1e77
Yuji Hamaguchi <yuji.hamaguchi@avanade.com>
2024-03-06 17:02:09 +0900

    add a test case

--- transforms_sample/utils.py
+++ transforms_sample/utils.py
@@ -11,7 +11,9 @@ def remove_leading_spaces(s: str) -> str:
     ("\tleading tab", "leading tab"),
     ("   \t mixed spaces and tab", "mixed spaces and tab"),
     ("no_leading_space", "no_leading_space"),
-    ("　全角スペース", "全角スペース")
+    ("　全角スペース", "全角スペース"),
+    ("\t　 全角スペース混合", "全角スペース混合")
+
 ])
 def test_remove_leading_whitespace(test_input, expected):
     assert remove_leading_spaces(test_input) == expected
